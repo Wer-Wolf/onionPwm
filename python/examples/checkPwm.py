e3707803434971b603f87896de1a59edf4f55625
Wer-Wolf <robo-armin@gmx.de>
2020-03-01 16:36:27 +0100

    improving stability

--- python/examples/checkPwm.py
+++ python/examples/checkPwm.py
@@ -7,12 +7,14 @@ led = pwm.OnionPwm(0)   # Initialize PWM channel 0 (GPIO 18)
 led.setFrequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
 led.setDutyCycle(0)    # Set the duty cycle of PWM channel 0 to 0%
 led.enable()    # Enables PWM channel 0 --> LED stays off
-while 1:
-    newCycle = input('Enter duty cycle (0 - 100) or EXIT')
-    if newCycle  is 'EXIT':
-        break
-    else:
-        led.setDutyCycle(float(newCycle))   # Changes the duty cycle of PWM channel 0
-        print('Current duty cycle: %3.10f', led.getDutyCycle())
-        print('As you can see, there is some slight difference due to rounding')
-led.disable()
+try:
+    while 1:
+        newCycle = input('Enter duty cycle (0 - 100) or EXIT: ')
+        if newCycle  == 'EXIT':
+            break
+        else:
+            led.setDutyCycle(float(newCycle))   # Changes the duty cycle of PWM channel 0
+            print('Current duty cycle: %3.10f' % led.getDutyCycle())
+            print('As you can see, there might be some slight difference due to rounding')
+finally:
+    led.disable()
