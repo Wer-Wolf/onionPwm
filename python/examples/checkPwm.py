5922b861ebed301f50b7cead1e753625fdd51baa
Wer-Wolf <robo-armin@gmx.de>
2020-03-05 20:53:57 +0100

    applying new library version

--- python/examples/checkPwm.py
+++ python/examples/checkPwm.py
@@ -3,18 +3,18 @@ import onionPwm as pwm
 # Dont forget enabling the pwm0 pin!
 # (docs.onion.io/omega2-docs/generating-pwm-signals.html -> Enabling PWM Pins)
 
-led = pwm.OnionPwm(0)   # Initialize PWM channel 0 (GPIO 18)
-led.setFrequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
-led.setDutyCycle(0)    # Set the duty cycle of PWM channel 0 to 0%
-led.enable()    # Enables PWM channel 0 --> LED stays off
-try:
-    while 1:
-        newCycle = input('Enter duty cycle (0 - 100) or EXIT: ')
-        if newCycle  == 'EXIT':
-            break
-        else:
-            led.setDutyCycle(float(newCycle))   # Changes the duty cycle of PWM channel 0
-            print('Current duty cycle: %3.10f' % led.getDutyCycle())
-            print('As you can see, there might be some slight difference due to rounding')
-finally:
-    led.disable()
+with pwm.OnionPwm(channel = 0) as led:  # Initialize PWM channel 0 (GPIO 18)
+    led.setFrequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
+    led.setDutyCycle(0)    # Set the duty cycle of PWM channel 0 to 0%
+    led.enable()    # Enables PWM channel 0 --> LED stays off
+    try:
+        while 1:
+            newCycle = input('Enter duty cycle (0 - 100) or EXIT: ')
+            if newCycle  == 'EXIT':
+                break
+            else:
+                led.setDutyCycle(float(newCycle))   # Changes the duty cycle of PWM channel 0
+                print('Current duty cycle: %3.10f' % led.getDutyCycle())
+                print('As you can see, there might be some slight difference due to rounding')
+    finally:
+        led.disable()
--- python/examples/led.py
+++ python/examples/led.py
@@ -3,16 +3,15 @@ import time
 
 # Dont forget enabling the pwm0 pin!
 # (docs.onion.io/omega2-docs/generating-pwm-signals.html -> Enabling PWM Pins)
+with pwm.OnionPwm(channel = 0) as led:  # Initialize PWM channel 0 (GPIO 18)
+    led.setFrequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
+    led.setDutyCycle(50)    # Set the duty cycle of PWM channel 0 to 50%
+    led.enable()    # Enables PWM channel 0 --> LED turns on
+    print('Wait 5 seconds, look at the LED')
+    time.sleep(5)
 
-led = pwm.OnionPwm(0)   # Initialize PWM channel 0 (GPIO 18)
-led.setFrequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
-led.setDutyCycle(50)    # Set the duty cycle of PWM channel 0 to 50%
-led.enable()    # Enables PWM channel 0 --> LED turns on
-print('Wait 5 seconds, look at the LED')
-time.sleep(5)
+    led.setDutyCycle(100)   # Set the duty cycle of PWM channel 0 to 100%
+    print('Wait 5 seconds, look at the LED again')
+    time.sleep(5)
 
-led.setDutyCycle(100)   # Set the duty cycle of PWM channel 0 to 100%
-print('Wait 5 seconds, look at the LED again')
-time.sleep(5)
-
-led.disable()   # Disables PWM channel 0 --> LED turns off
+    led.disable()   # Disables PWM channel 0 --> LED turns off
