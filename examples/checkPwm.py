5eef972b052c3d8e01e604cb27552d0b7101c51f
Wer-Wolf <robo-armin@gmx.de>
2020-09-02 17:44:50 +0200

    Updating examples

--- examples/checkPwm.py
+++ examples/checkPwm.py
@@ -4,8 +4,8 @@ import onionPwm as pwm
 # (docs.onion.io/omega2-docs/generating-pwm-signals.html -> Enabling PWM Pins)
 
 with pwm.OnionPwm(channel=0) as led:  # Initialize PWM channel 0 (GPIO 18)
-    led.setFrequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
-    led.setDutyCycle(0)    # Set the duty cycle of PWM channel 0 to 0%
+    led.set_frequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
+    led.set_duty_cycle(0)    # Set the duty cycle of PWM channel 0 to 0%
     led.enable()    # Enables PWM channel 0 --> LED stays off
     try:
         while 1:
@@ -13,8 +13,8 @@ with pwm.OnionPwm(channel=0) as led:  # Initialize PWM channel 0 (GPIO 18)
             if newCycle == 'EXIT':
                 break
             else:
-                led.setDutyCycle(float(newCycle))   # Changes the duty cycle of PWM channel 0
-                print('Current duty cycle: %3.10f' % led.getDutyCycle())
+                led.set_duty_cycle(float(newCycle))   # Changes the duty cycle of PWM channel 0
+                print('Current duty cycle: %3.10f' % led.get_duty_cycle())
                 print('As you can see, there might be some slight difference due to rounding')
     finally:
         led.disable()
--- examples/led.py
+++ examples/led.py
@@ -4,13 +4,13 @@ import time
 # Dont forget enabling the pwm0 pin!
 # (docs.onion.io/omega2-docs/generating-pwm-signals.html -> Enabling PWM Pins)
 with pwm.OnionPwm(channel=0) as led:  # Initialize PWM channel 0 (GPIO 18)
-    led.setFrequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
-    led.setDutyCycle(50)    # Set the duty cycle of PWM channel 0 to 50%
+    led.set_frequency(1000)  # Set the Frequency of PWM channel 0 to 1000 Hz
+    led.set_duty_cycle(50)    # Set the duty cycle of PWM channel 0 to 50%
     led.enable()    # Enables PWM channel 0 --> LED turns on
     print('Wait 5 seconds, look at the LED')
     time.sleep(5)
 
-    led.setDutyCycle(100)   # Set the duty cycle of PWM channel 0 to 100%
+    led.set_duty_cycle(100)   # Set the duty cycle of PWM channel 0 to 100%
     print('Wait 5 seconds, look at the LED again')
     time.sleep(5)
 
